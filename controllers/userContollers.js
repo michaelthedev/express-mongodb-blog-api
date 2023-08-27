d884215b9d24574a984711f6d28ed5ca7fb7e6b3
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:38:46 +0100

    Whitespaces and Indentation for `getAllUsers`

--- controllers/userContollers.js
+++ controllers/userContollers.js
@@ -9,8 +9,15 @@ const User = require('../models/userModel')
 const getAllUsers = async(req, res)=>{
     try {
         let users = await User.find()
-    if(!users){
-        return res.status(404).json({message: 'No users found'})
+        if (!users) {
+            return res.status(404)
+                .json({message: 'No users found'})
+        }
+
+        return res.status(200)
+            .json({users})
+    } catch (error) {
+        console.log(error.message);
     }
     return res.status(200).json({users})
     } catch (error) {
