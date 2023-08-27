4965f50daab0db7843797a8169e381b7e2ab755a
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:39:30 +0100

    Added `getSingleUser` method

--- controllers/userContollers.js
+++ controllers/userContollers.js
@@ -20,6 +20,23 @@ const getAllUsers = async(req, res)=>{
         console.log(error.message);
     }
 }
+
+/**
+ * Get single user by email
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+const getSingleUser = async(req, res)=>{
+    try {
+        let {email} = req.body;
+        let users = await User.findOne({email})
+        if (!users) {
+            return res.status(404)
+                .json({message: 'User was not found'})
+        }
+        return res.status(200)
+            .json({users})
     } catch (error) {
         console.log(error.message);
     }
