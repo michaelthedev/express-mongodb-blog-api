917740abf4df87b3ad713b9f7eba8bd5e48a5845
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:31:43 +0100

    Added `signup` method

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,3 +28,27 @@ const login = async(req,res)=>{
     }
 }
 
+/**
+ * User Sign Up
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+const signup = async(req,res)=>{
+    const { name, email, password } = req.body
+    try {
+        let existingUser = await User.findOne({email})
+        if (existingUser) {
+            return res.status(400)
+                .json({
+                    message:'User already exists. Login instead'
+                })
+        }
+
+        let newUser = await User.create({name, email, password, blog: []})
+        return res.status(201)
+            .json({newUser})
+    } catch (error) {
+        console.log(error.message);
+    }
+}
