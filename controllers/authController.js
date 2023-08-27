5333b2dcc60cb5a99d62914bd759152246c3178b
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:31:31 +0100

    Added `login` method

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,3 +5,26 @@
  */
 
 const User = require('../models/userModel');
+
+/**
+ * User Login
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
+const login = async(req,res)=>{
+    const { email, password } = req.body
+    try {
+        await User.comparePassword(email, password)
+        return res.status(200)
+            .json({
+                message:'You are now logged In!'
+            })
+    } catch (error) {
+        return res.status(404)
+            .json({
+                message:error.message
+            })
+    }
+}
+
