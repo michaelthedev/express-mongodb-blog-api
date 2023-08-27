66281e92683591d4568db5a521f8d11bef037004
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:51:29 +0100

    Added /auth routes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
 const userRoutes = require("./routes/userRoutes");
 const blogRoutes = require("./routes/blogRoutes");
+const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/blog", blogRoutes);
 
