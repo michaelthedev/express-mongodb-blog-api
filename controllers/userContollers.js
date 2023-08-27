fe3faace9d89a66309cab2cee55eb3678162374e
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-08-27 15:32:11 +0100

    Removed `signUp` method

--- controllers/userContollers.js
+++ controllers/userContollers.js
@@ -11,19 +11,6 @@ const getAllUsers = async(req,res)=>{
         console.log(error.message);
     }
 }
-const signUp = async(req,res)=>{
-    const {name, email, password} =req.body
-    try {
-        let existingUser = await User.findOne({email})
-        if(existingUser){
-            return res.status(400).json({message:'User already exists. Login instead'})
-        }
-        let newUser = await User.create({name, email, password, blog: []})
-        return res.status(201).json({newUser})
-    } catch (error) {
-        console.log(error.message);
-    }
-}
 const logIn = async(req,res)=>{
     const { email, password} =req.body
     try {
