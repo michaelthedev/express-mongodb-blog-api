197178bd54e4063dbc9b3a307f528e8fd49020ba
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-10 11:28:57 +0100

    Changed env var names, console port number

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/blog", blogRoutes);
 
-mongoose.connect(process.env.dbURI).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   app.listen(process.env.PORT || 3000, () => {
-    console.log("listening");
+    console.log("listening on port "+(process.env.PORT || 3000));
   });
 });
